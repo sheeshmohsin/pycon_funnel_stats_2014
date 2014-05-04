00def6879b20760e5706e6e6b3bc3a62667b9fb6
Sheesh Mohsin <sheeshmohsin@gmail.com>
2014-05-04 11:53:26 +0530

    added google api for pie chart

--- pycon_funnel_stats_2014/statistics/views.py
+++ pycon_funnel_stats_2014/statistics/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
 from django.utils import timezone
-
+from urllib2 import urlopen
+import program
 
 def stats(request):
-	return render_to_response("home.html")
+	return render_to_response("home.html",{'total':8, 'begineer':2, 'intermediate':3, 'advanced':5})
