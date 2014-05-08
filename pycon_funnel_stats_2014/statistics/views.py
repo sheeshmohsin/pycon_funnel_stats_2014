e066767a0421357b01d78443859b3e69166d2cc2
Sheesh Mohsin <sheeshmohsin@gmail.com>
2014-05-08 23:49:26 +0530

    added the function beginner into home.html

--- pycon_funnel_stats_2014/statistics/views.py
+++ pycon_funnel_stats_2014/statistics/views.py
@@ -13,8 +13,9 @@ def stats(request):
     total = program.total(page)
     proposallist = program.proposallist(page)
     section = program.section(page)
+    beginnerlist = program.beginner(page)
     context = Context({'total':total, 'begineer':proposallist[0],
             'intermediate': proposallist[1],'advanced':proposallist[2], 
             'beginnerchart': section[0], 'intermediatechart': section[1], 
-            'advancedchart': section[2]})
+            'advancedchart': section[2], 'beginners' : beginnerlist})
     return render_to_response("home.html", context)
