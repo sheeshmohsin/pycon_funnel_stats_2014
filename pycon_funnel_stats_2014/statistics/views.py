e45fb91d12a2f39a66d54dfbd8eaa635bb21054b
Sheesh Mohsin <sheeshmohsin@gmail.com>
2014-05-10 23:52:44 +0530

    Added intermediate list (section-wise)

--- pycon_funnel_stats_2014/statistics/views.py
+++ pycon_funnel_stats_2014/statistics/views.py
@@ -14,11 +14,17 @@ def stats(request):
     proposallist = program.proposallist(page)
     section = program.section(page)
     beginnerlist = program.beginner(page)
+    intermediatelist = program.intermediate(page)
     context = Context({'total':total, 'begineer':proposallist[0],
             'intermediate': proposallist[1],'advanced':proposallist[2], 
             'beginnerchart': section[0], 'intermediatechart': section[1], 
             'advancedchart': section[2], 'corebeginners' : beginnerlist[0],
             'embeddedbeginners' : beginnerlist[1], 'infrabeginners' : beginnerlist[2],
             'scibeginners' : beginnerlist[3], 'softbeginners' : beginnerlist[4],
-            'webbeginners' : beginnerlist[5], 'workbeginners' : beginnerlist[6]})
+            'webbeginners' : beginnerlist[5], 'workbeginners' : beginnerlist[6],
+            'coreintermediates' : intermediatelist[0], 'embeddedintermediates' : intermediatelist[1],
+            'infraintermediates' : intermediatelist[2], 'sciintermediates' : intermediatelist[3],
+            'softintermediates' : intermediatelist[4], 'webintermediates' : intermediatelist[5],
+            'workintermediates' : intermediatelist[6]})
     return render_to_response("home.html", context)
+
